35a52e92ff36cb2c09fcb519f2caddfc663774ec
colorlight <colorlightleecaa@gmail.com>
2017-07-17 16:45:26 +0800

    添加vue的时间模式化输出

---
+++ datejs/date.js
@@ -0,0 +1,25 @@
+function formatDate(date, fmt) {
+  var tempMatch = fmt.match(/y+/);
+  if (tempMatch) {
+		fmt = fmt.replace(tempMatch[0], (date.getFullYear() + '').substr(4 - tempMatch[0].length));
+	}
+	let o = {
+		'M+': date.getMonth() + 1,
+		'd+': date.getDate(),
+		'h+': date.getHours(),
+		'm+': date.getMinutes(),
+		's+': date.getSeconds()
+	};
+	for (let k in o) {
+	  tempMatch = fmt.match(new RegExp(k));
+		if (tempMatch) {
+			let str = o[k] + '';
+			fmt = fmt.replace(tempMatch[0], (tempMatch[0].length === 1) ? str : padLeftZero(str));
+		}
+	}
+	return fmt;
+};
+
+function padLeftZero(str) {
+	return ('00' + str).substr(str.length);
+}
